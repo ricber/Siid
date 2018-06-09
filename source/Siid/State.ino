fd1f5b95e4ecdbdf80e6b329d792813482114d84
Riccardo Bertoglio <ticio94@hotmail.it>
2018-06-09 12:22:58 +0200

    Added sonar test code and some other states to the State sketch

--- source/Siid/State.ino
+++ source/Siid/State.ino
@@ -1,21 +1,26 @@
 // #### DEFINITIONS ####
 #define LK_TIME_OUT 10000 // LOOK_AROUND timeout
 #define FW_TIME_OUT 5000 // FORWARD timeout
+#define SAD_TIME_OUT 5000 // sad animation timeout
 
 // #### ROBOT STATE ####
-enum State_enum {LOOK_AROUND, PERSON_DET, FORWARD, STOP_CHECK}; // all possible states of the robot
-enum Sensors_enum {NONE, THERMOSENSOR, FRONT_SONAR}; // all possible sensors inputs
-uint8_t current_state = LOOK_AROUND;           // current state of the robot
-uint8_t previous_state = LOOK_AROUND;          // previous state of the robot
+enum State_enum {LOOK_AROUND, PERSON_DET, FORWARD, STOP_CHECK, ROTATE_SAD}; // all possible states of the robot
+/* all possible sensors inputs
+ * THERMOSENSOR_UP means that it has detected a person
+ * THERMOSENSOR_DWN means that the obstacle is an object
+ */
+enum Sensors_enum {NONE, THERMOSENSOR_UP, THERMOSENSOR_DWN, FRONT_SONAR}; 
+byte current_state = LOOK_AROUND;           // current state of the robot
+byte previous_state = LOOK_AROUND;          // previous state of the robot
 
 
 
 // #### TIME ####
-unsigned long starting_time_state;              // state beginning execution time
+unsigned long starting_time_state;          // state beginning execution time
 
 
 // state machine that controls the robot. Based on the current state and on the input it performs certain outputs and goes in the next state. 
-void state_machine(uint8_t sensors){
+void state_machine(byte sensors){
   switch(current_state){
     case LOOK_AROUND:
       /* LOOK AROUND
@@ -24,7 +29,7 @@ void state_machine(uint8_t sensors){
        *  Internal petals are closed
        *  External petals are opened
        */
-       if (sensors == THERMOSENSOR){
+       if (sensors == THERMOSENSOR_UP){
         setState(PERSON_DET); // person detected
         
        }
@@ -61,8 +66,27 @@ void state_machine(uint8_t sensors){
       * In this state the robot first stops the wheels movement
       * and then it checks what is in front of it, a person or an obstacle
       */
+      if (sensors == THERMOSENSOR_DWN) {
+        setState(ROTATE_SAD);
+        
+      }
+      else if (sensors == THERMOSENSOR_UP) {
+        setState(PERSON_DET);
+      }
         
         break;
+     case ROTATE_SAD:
+        /* ROTATE
+         * In this state the robot turn randomly right or left to avoid the obstacle
+         * Then it starts the sad animation because it likes to interact with people and so it's sad when approches an obstacle 
+         */
+         if (millis() - starting_time_state > SAD_TIME_OUT) {
+            setState(LOOK_AROUND);
+         }
+         else if (sensors == FRONT_SONAR) {
+            setState(STOP_CHECK);
+         }         
+         break;
     default:
       break;
   }
@@ -70,7 +94,7 @@ void state_machine(uint8_t sensors){
 
 
 // sets the current state of the robot and saves the previous one
-void setState(uint8_t state) {
+void setState(byte state) {
   previous_state = current_state;
   current_state = state;
   starting_time_state = millis();
---
+++ testing/sonars/sonars.ino
@@ -0,0 +1,18 @@
+#include <NewPing.h>
+ 
+#define TRIGGER_PIN 11
+#define ECHO_PIN 12
+#define MAX_DISTANCE 400
+ 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+ 
+void setup() {
+   Serial.begin(9600);
+}
+ 
+void loop() {
+   delay(50);
+   unsigned int uS = sonar.ping_cm();
+   Serial.print(uS);
+   Serial.println("cm");
+}
