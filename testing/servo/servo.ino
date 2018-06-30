2d947d45ebc33859be9cf37b3bab6a194a2ca7e0
Riccardo Bertoglio <ticio94@hotmail.it>
2018-06-30 12:03:23 +0200

    Update testing servo code

--- testing/servo/servo.ino
+++ testing/servo/servo.ino
@@ -5,17 +5,15 @@ Servo myservo; // create servo object to control a servo
 
 void setup()
 {
+    Serial.begin(115200);
     myservo.attach(22); // attaches the servo on pin 9 to the servo object
 }
 
 void loop()
 {
-    for (int i = 0; i < 5; i++){
-        myservo.write(0);
-        delay(2000); // waits for the servo to get there
-        myservo.write(85);
-        delay(2000);
-    }
-     // sets the servo position according to the scaled value
-    
+    for (int i = 85; i > 30; i--){
+        myservo.write(i);
+        delay(200); // waits for the servo to get there
+        Serial.println(i);
+    }    
 }
